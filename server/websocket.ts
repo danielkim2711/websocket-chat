e1b25fbbe04a106b01af80ba5fb0a138bee2409a
Daniel Kim <danielkim2711@gmail.com>
2025-02-09 22:15:13 +1300

    docs: remove comment

--- server/websocket.ts
+++ server/websocket.ts
@@ -6,7 +6,6 @@ wss.on('connection', (ws) => {
   console.log('New client connected');
 
   ws.on('message', (message) => {
-    // 연결된 모든 클라이언트에게 메시지 브로드캐스트
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === ws.OPEN) {
         client.send(message.toString());
